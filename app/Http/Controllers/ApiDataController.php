644f14f58caa570a441d2fae5cd8237b9c8762ef
codesyariah122 <pujiermanto@gmail.com>
2021-08-25 22:34:01 +0700

    update api data controller member_list method orderby achievements

--- app/Http/Controllers/ApiDataController.php
+++ app/Http/Controllers/ApiDataController.php
@@ -426,6 +426,7 @@ class ApiDataController extends Controller
         $members = User::join('profile', 'profile.user_id', '=', 'users.id')
                     ->where('roles', '=', json_encode(['MEMBER']))
                     ->whereIn('users.id', [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18])
+                    ->orderBy('achievements', 'DESC')
                     // ->get();
                     ->paginate(6);
 

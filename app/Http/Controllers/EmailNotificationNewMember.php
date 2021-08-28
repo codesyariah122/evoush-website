afbc812bc39de65c2cbb897e24bb4c6f8234d78b
codesyariah122 <pujiermanto@gmail.com>
2021-08-29 00:30:00 +0700

    update EmailNotificationForNewMemberJoin::Controller

--- app/Http/Controllers/EmailNotificationNewMember.php
+++ app/Http/Controllers/EmailNotificationNewMember.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Validator;
+// use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Mail\ActivationNewMemberMail;
 use App\Models\User;
@@ -17,17 +17,21 @@ class EmailNotificationNewMember extends Controller
     //
     public function send(Request $request)
     {
-        $validation = \Validator::make($request->all(),[
-           "name" => "required|min:5|max:100",
-           "email" => "required|email|unique:users",
-           "phone" => "required|digits_between:10,15",
-           "province" => "required",
-           "city" => "required",
-           "password" => "required",
-           "password_confirmation" => "required|same:password"
-       ])->validate();
+        $validation = Validator::make($request->all(),[
+         "name" => "required|min:5|max:100",
+         "email" => "required|email|unique:users",
+         "phone" => "required|max:20",
+         "province" => "required",
+         "city" => "required",
+         "password" => "required",
+         "password_confirmation" => "required|same:password"
+        ]);
 
-       	$username_path = $request->get('username_path');
+        if ($validation->fails()) {
+            return response()->json($validation->errors(), 400);
+        }
+
+        $username_path = $request->get('username_path');
 
         $new_user = new User;
         $new_user->name = strtolower($request->get('name'));
@@ -51,8 +55,8 @@ class EmailNotificationNewMember extends Controller
         $url = 'https://dev.farizdotid.com/api/daerahindonesia/provinsi/'.$request->get('province');
         $prov = file_get_contents($url);
         $data_prov = json_decode($prov, 1);
-        // echo $data_prov['nama'];
-        // die;
+          // echo $data_prov['nama'];
+          // die;
         $provinsi = $data_prov['nama'];
         $new_userid = $new_user->id;
         $new_username = $new_user->username;
@@ -64,27 +68,27 @@ class EmailNotificationNewMember extends Controller
         $new_profile->save();
 
         $new_member = new Member;
-        // $new_member->username = $new_username;
+          // $new_member->username = $new_username;
         $new_member->user_id = $new_userid;
         $new_member->sponsor_id = $request->get('sponsor_id');
         $new_member->save();
 
         $new_join = new Joining;
-        // $new_join->username = $new_username;
+          // $new_join->username = $new_username;
         $new_join->member_id = $new_member->id;
         $new_join->user_id = $new_member->user_id;
         $new_join->save();
 
         $sponsor = User::join('profile', 'users.id', '=', 'profile.user_id')
-        // ->where('roles',  json_encode("MEMBER"))
+          // ->where('roles',  json_encode("MEMBER"))
         ->where('users.id', $new_member->sponsor_id)
         ->get(['profile.*', 'users.*']);
 
         $details = [
-            'title' => 'You Have New Member Join',
-            'url' => 'https://evoush.com',
-            'username' => $new_user->username,
-            'sponsor' => $sponsor
+          'title' => 'You Have New Member Join',
+          'url' => 'https://evoush.com',
+          'username' => $new_user->username,
+          'sponsor' => $sponsor
         ];
 
         // return response()->json([
@@ -93,19 +97,20 @@ class EmailNotificationNewMember extends Controller
         // ]);
 
         try {
-
-            Mail::to('admin_evoush@evoush.com')->send(new ActivationNewMemberMail($details));
-            return response()->json([
-            	'message' => '<b class="text-info">'.$new_user->username.'</b> proses join anda sudah dikirim ke pihak sponsor <b class="text-primary">'.$sponsor[0]->username.',</b> untuk selanjutnya menunggu  di aktivasi oleh sponsor anda.',
-            	'data_sponsor' => $sponsor,
-                'data_member' => $new_user
-            ], Response::HTTP_OK);
-
+          Mail::to('admin_evoush@evoush.com')->send(new ActivationNewMemberMail($details));
+          return response()->json([
+           'message' => '<b class="text-info">'.$new_user->username.'</b> proses join anda sudah dikirim ke pihak sponsor <b class="text-primary">'.$sponsor[0]->username.',</b> untuk selanjutnya menunggu  di aktivasi oleh sponsor anda.',
+           'data_sponsor' => $sponsor,
+           'data_member' => $new_user
+         ], Response::HTTP_OK);
 
 
 
-        } catch(\Exception $e){
-            echo "Email gagal dikirim karena $e.";
+        } catch(Exception $e){
+          return response()->json([
+            'message' => $e->getMessage()
+          ]);
+        // echo "Email gagal dikirim karena $e.";
         }
 
     }

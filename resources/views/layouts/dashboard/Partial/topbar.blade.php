7398641c0a62e86fe8d8fcdb11ca181f1546ca4b
codesyariah122 <pujiermanto@gmail.com>
2021-11-01 16:14:41 +0700

    update topbar

--- resources/views/layouts/dashboard/Partial/topbar.blade.php
+++ resources/views/layouts/dashboard/Partial/topbar.blade.php
@@ -172,8 +172,8 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->username }}</span>
                                 @if(Auth::user()->avatar)
-                                    {{-- <img src="{{asset('storage/'.Auth::user()->avatar)}}" width="30px"/>  --}}
-                                    <img src="https://raw.githubusercontent.com/evoush-products/bahan_evoush/master/migration_db/{{ Auth::user()->avatar }}" class="img-fluid" width="30">
+                                    <img src="{{asset('storage/'.Auth::user()->avatar)}}" width="30"/>
+                                   {{--  <img src="https://raw.githubusercontent.com/evoush-products/bahan_evoush/master/migration_db/{{ Auth::user()->avatar }}" class="img-fluid" width="30"> --}}
                                 @else
                                     <img src="https://raw.githubusercontent.com/codesyariah122/bahan-evoush/master/images/profile/default.jpg" class="img-responsive rounded-circle" width="50" height="50">
                                 @endif
